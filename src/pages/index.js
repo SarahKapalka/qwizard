e1b4b12a5b65e02978ebc15c01b1d54711ca727f
SarahKapalka <sarahnj73bc@gmail.com>
2023-03-10 01:39:34 +0000

    removed fetch from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.scss';
 import Post from '@/components/Post';
 import Layout from '@/components/Layout';
+import Quiz from '../../models/Quiz';
 
-export default function Home({quizes}) {
-
+export default function Home({quiz}) {
+const quizList =JSON.parse(quiz)
   return (
     <Layout page="Homepage">
         <main className={styles.main}>
           {
-            quizes.map((x,i)=>{
+            quizList.map((x,i)=>{
               return(
               <Post key={i} quizData={x} i={i}/>
               )
@@ -23,16 +24,20 @@ export default function Home({quizes}) {
 
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
-  const res = await fetch('http://localhost:3000/api/Quiz', {
-    method: 'GET',
-  })
-  const quizes = await res.json()
-
+  let quiz;
+  try{
+    const quizes = await Quiz.find({}).then((data) => {
+            quiz= JSON.stringify(data);
+            console.log(typeof quiz);
+      }); 
+    }catch(error) {
+    console.log({ data : 'loading'}, error); 
+    }
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
   return {
     props: {
-      quizes,
+      quiz,
     },
   }
 }
