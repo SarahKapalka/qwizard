20b6e3ee2e72c7ea0d89f4aa1b4698f6762a4619
SarahKapalka <sarahnj73bc@gmail.com>
2023-03-10 00:33:35 +0000

    tried removing thens

--- src/pages/update/[quiz].js
+++ src/pages/update/[quiz].js
@@ -3,10 +3,11 @@ import Layout from '@/components/Layout';
 
 // Generates `/posts/1` and `/posts/2`
 export async function getStaticPaths() {
-  let paths;
   const res = await fetch('https://qwizard-ten.vercel.app/api/Quiz', {
   method: 'GET'
-  }).then(response => response.json()).then(quizes => paths = quizes.map((quiz)=>{return {params: {quiz: quiz._id}}}))
+  })
+  const quizes = await res.json()
+  const paths = quizes.map((quiz)=>{return {params: {quiz: quiz._id}}})
 
     return {
       paths,
@@ -16,10 +17,10 @@ export async function getStaticPaths() {
   
   // `getStaticPaths` requires using `getStaticProps`
   export async function getStaticProps(context) {
-    let quiz;
     const res = await fetch(`https://qwizard-ten.vercel.app/api/Quiz?id=${context.params.quiz}`, {
         method: 'GET'
-      }).then(response => response.json()).then(quizes => quiz = quizes)
+      })
+      const quiz = await res.json()
       // By returning { props: { posts } }, the Blog component
       // will receive `posts` as a prop at build time
       return {
